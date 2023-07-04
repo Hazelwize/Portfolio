40c5a37c8925d525ce420b59faa8e17177a82f9f
hazelwize <thehazelwize@gmail.com>
2023-07-03 22:43:01 -0400

    add react build in app folder

--- app/portsite/src/components/Main.js
+++ app/portsite/src/components/Main.js
@@ -1,5 +1,32 @@
 const Main = () => {
     return (
-        
+        <div class="backGrid-1">
+				<header>
+					<nav class="flex navBar">
+						<ul class="flex navMenu">
+							<li class="navItem"><a class="navLink" href="#about">About Me</a></li>
+							<li class="navItem"><a class="navLink" href="#portfolio">Portfolio</a></li>
+							<li class="navItem"><a class="navLink" href="#contact">Contact</a></li>
+						</ul>
+						<div class="hamburger">
+							<span class="bar"></span>
+							<span class="bar"></span>
+							<span class="bar"></span>
+						</div>
+					</nav>
+				</header>
+				<section class="flex grid-1">
+					<section class="block-1 ">
+						<h1>Welcome to your digital journey!</h1>
+						<p>Hello, my name is Micah aka Hazelwize. I am a software engineer with a passion for creating full stack web applications. I enjoy bringing local businesses into the digital age with responsive and intuitive web design. </p>
+						<a class="contact"  href="#contact">Contact Me</a>
+					</section>
+					<section class="block-1-2">
+					
+					</section>	
+				</section>
+			</div>	
     )
 }
+
+export default Main
