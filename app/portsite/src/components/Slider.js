b39061afee64d526c4fb17a0f4965970b483478f
Micah Jackson <thehazelwize@gmail.com>
2023-07-06 18:20:29 -0400

    add default styling before customization #11

--- app/portsite/src/components/Slider.js
+++ app/portsite/src/components/Slider.js
@@ -2,7 +2,7 @@ const Slider = ({color, colorValue, setColorValue}) => {
     return (
         <div style={{backgroundColor: color.section, borderColor: color.border, color: color.text}}className="slideWrapper">
             <div className="slideContainer">
-                <h3 style={{color: color.text}}>Slide to change the color scheme:</h3>
+                <h3 className="sliderTitle" style={{color: color.text}}>Slide to change the color scheme:</h3>
                 <input type="range" min="1" max="360" value={colorValue} onChange={(e) => setColorValue(e.target.value)} className="slider" id="myRange"></input>
             </div>
         </div>
