67f6527c8adc2e2c71d5be80d42abe88d30b0d39
Micah Jackson <thehazelwize@gmail.com>
2023-07-06 20:03:37 -0400

    add details section with custom styling #10

--- app/portsite/src/components/Main.js
+++ app/portsite/src/components/Main.js
@@ -1,11 +1,11 @@
 import HeaderNav from './HeaderNav'
 
-const Main = ({color, showSlider, setShowSlider}) => {
+const Main = ({color, border, showSlider, setShowSlider}) => {
     return (
         <div className="backGrid-1">
 			<HeaderNav color={color} showSlider={showSlider} setShowSlider={setShowSlider}/>
 			<section style={{borderColor: color.border}}className="flex grid-1">
-				<section style={{borderColor: color.border, backgroundColor: color.section}} className="block-1 ">
+				<section style={{borderColor: color.border, backgroundColor: color.section , borderRadius: `${border}%` }} className="block-1 ">
 					<h1 style={{color: color.text}}>Welcome to your digital journey!</h1>
 					<p style={{color: color.text}}>Hello, my name is Micah aka Hazelwize. I am a software engineer with a passion for creating full stack web applications. I enjoy bringing local businesses into the digital age with responsive and intuitive web design. </p>
 				</section>
